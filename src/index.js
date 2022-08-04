1ed4edf411e8ac90f3df12d085e7b4a4553e5222
mpandit-inContact <101241953+mpandit-inContact@users.noreply.github.com>
2022-08-04 16:58:50 +0530

    Created Demo Login Page
    Created Demo Login Page

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Route } from 'react-router-dom';
+import './index.css'; 
+import LoginUi from './Login Ui/LoginUi';
+
+
+
+ReactDOM.render( 
+    <LoginUi /> ,   
+  document.getElementById('root')
+);
+ 
